12d9ca3cb5977845335c406ba14458b7f92aa044
Jim Larson <jimlarson@google.com>
2018-12-03 11:30:57 -0800

    Remove protos which are now in googleapis.
    Update to latest versions of bazel rules for Go.
    Pin to a fixed version of googleapis.

--- tools/proto_utils_lib_test.cc
+++ tools/proto_utils_lib_test.cc
@@ -6,7 +6,7 @@
 #include "gmock/gmock.h"
 #include "gtest/gtest.h"
 #include "absl/strings/str_cat.h"
-#include "proto/v1/value.pb.h"
+#include "google/api/expr/v1alpha1/value.pb.h"
 #include "tools/tests/test_message1.pb.h"
 #include "tools/tests/test_value.pb.h"
 
