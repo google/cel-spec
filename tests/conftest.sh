a99c94d596a912d50fda9e46bf9cad24658302f6
Jim Larson <jimlarson@google.com>
2018-12-19 13:26:37 -0800

    Additional debugging.

--- tests/conftest.sh
+++ tests/conftest.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
+echo "Running test binary as " "$@"
+echo "Current directory " "$(pwd)"
+echo "Path is" "$PATH"
+echo "Server binary:" "$(ls -l server/cel_server)"
+echo "Contents are" "$(find -L -type f | xargs namei)"
+echo "Basic textproto:"
+echo =====
+# cat external/com_google_cel_spec/tests/simple/testdata/basic.textproto || echo no such file
+echo =====
 exec "$@"
