cf40f937825f6b04d7a48dc2118a7a2ea9682ba4
Sokwhan Huh <sokwhan@google.com>
2024-03-20 20:22:09 +0000

    Update cloudbuild to use bazel 6.4.0

--- regen_go_proto_canonical_protos.sh
+++ regen_go_proto_canonical_protos.sh
@@ -7,6 +7,5 @@ mkdir ./expr
 files=($(bazel aquery 'kind(proto, //proto/cel/expr:all)' | grep Outputs | grep "[.]pb[.]go" | grep "expr_go_proto" | sed 's/Outputs: \[//' | sed 's/\]//' | tr "," "\n"))
 for src in ${files[@]};
 do
-  dst=$(echo $src | sed 's/\(.*\%\/github.com\/google\/cel-spec\/\(.*\)\)/\2/')
   $(cp $src ./expr)
 done
