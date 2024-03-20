8cfc3cd1342c9260b5d77127a4200922a7265d6c
Sokwhan Huh <sokwhan@google.com>
2024-03-20 22:47:55 +0000

    Update regen_go_proto_canonical_protos.sh

--- regen_go_proto_canonical_protos.sh
+++ regen_go_proto_canonical_protos.sh
@@ -7,5 +7,5 @@ mkdir ./expr
 files=($(bazel aquery 'kind(proto, //proto/cel/expr:all)' | grep Outputs | grep "[.]pb[.]go" | grep "expr_go_proto" | sed 's/Outputs: \[//' | sed 's/\]//' | tr "," "\n"))
 for src in ${files[@]};
 do
-  $(cp $src ./expr)
+  cp -v "${src}" ./expr/
 done
