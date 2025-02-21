74a79fc43f0313650e221c0b3687f384da0ffd83
Aakash Chauhan <aakashchauhan@google.com>
2025-02-21 12:18:50 +0000

    added test suite messages to simple.proto and updated test_config to env_config

--- conformance/test/simple.pb.go
+++ conformance/test/simple.pb.go
@@ -669,6 +669,80 @@ func file_cel_expr_conformance_test_simple_proto_init() {
 	if File_cel_expr_conformance_test_simple_proto != nil {
 		return
 	}
+	if !protoimpl.UnsafeEnabled {
+		file_cel_expr_conformance_test_simple_proto_msgTypes[0].Exporter = func(v any, i int) any {
+			switch v := v.(*SimpleTestFile); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cel_expr_conformance_test_simple_proto_msgTypes[1].Exporter = func(v any, i int) any {
+			switch v := v.(*SimpleTestSection); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cel_expr_conformance_test_simple_proto_msgTypes[2].Exporter = func(v any, i int) any {
+			switch v := v.(*SimpleTest); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cel_expr_conformance_test_simple_proto_msgTypes[3].Exporter = func(v any, i int) any {
+			switch v := v.(*TypedResult); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cel_expr_conformance_test_simple_proto_msgTypes[4].Exporter = func(v any, i int) any {
+			switch v := v.(*ErrorSetMatcher); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_cel_expr_conformance_test_simple_proto_msgTypes[5].Exporter = func(v any, i int) any {
+			switch v := v.(*UnknownSetMatcher); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+	}
 	file_cel_expr_conformance_test_simple_proto_msgTypes[2].OneofWrappers = []any{
 		(*SimpleTest_Value)(nil),
 		(*SimpleTest_TypedResult)(nil),
